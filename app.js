47674ed6380302e7149344d3fa3cabedcd07bf81
Alysson Melo <alyssoncm@gmail.com>
2023-05-15 22:13:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-      <h1>Welcome to Back4app Containers</h1>
-    </div>
+    <html>
+      <head>
+        <style>
+          @font-face {
+            font-family: "Sora";
+            src: url("https://example.com/fonts/sora/Sora-SemiBold.ttf");
+            font-style: normal;
+            font-weight: 600;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: "Sora";
+            src: url("https://example.com/fonts/sora/Sora-Bold.ttf");
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+          }
+          /* Rest of your CSS here... */
+          body {
+            background-color: #1a1a1a;
+            color: #fff;
+            font-family: Sora, Arial, sans-serif;
+            text-align: center;
+            height: 100vh;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to Back4app Containers</h1>
+      </body>
+    </html>
   `);
 });
 
